be38e819850a28e20696bfae9688123b34f4adbc
SimranAhuja00 <97106836+SimranAhuja00@users.noreply.github.com>
2023-04-08 15:58:39 +0530

    Add files via upload
    learning functions in the file system(fs) module in node js.

---
+++ index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+//creating a new file
+fs.writeFileSync('read.txt',"It is a good day today.");
+fs.writeFileSync('read.txt'," Very pleasant weather."); //overwrites on existing file
+
+//add something to existing file
+fs.appendFileSync('read.txt', " Taking a walk outside should be fun.");
+
+//to read a file
+const buffer_data = fs.readFileSync('read.txt');
+console.log(buffer_data);        //prints buffer data
+
+//converting buffer data to String format
+
+const data = buffer_data.toString();
+console.log(data);
+
+//to rename the file
+fs.renameSync('read.txt','readwrite.txt');
